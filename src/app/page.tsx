9ca2228c1adce7638ea192ff478ace5ae478afeb
sumin8838 <dudtladl0573@naver.com>
2025-09-24 10:25:17 +0900

    2nd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome Home!</h1>
-      <p>Hello world</p>
+      <h1 className="text-2xl font-bold">Welcome Home! </h1>
+      <p> clerk.com을 이용한 인증 </p>
     </div>
   )
 }
